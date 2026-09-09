44e412223154d800ae2f23f1f50efe283a811c67
Yamim Ali <nnjjstudiojj@gmail.com>
2026-09-09 08:48:39 +0100

    Made the constructor and the first versions of the identity, rotation and scaling matrices before tests

--- src/main/java/com/nnjjstudiojj/physics/Matrix2.java
+++ src/main/java/com/nnjjstudiojj/physics/Matrix2.java
@@ -2,20 +2,37 @@ package com.nnjjstudiojj.physics;
 
 public class Matrix2 {
 
-    private float a;
-    private float b;
-    private float c;
-    private float d;
+    private float[][] matrix;
 
-    private float[][] matrix = new float[][] {{a, c}, {b, d}};
+    public Matrix2(float a, float b, float c, float d) {
 
-    public float[][] Matrix2(float a, float b, float c, float d) {
+        matrix = new float[][] {
+            {a, b},
+            {c, d}
+        };
+    }
 
-        this.a = a;
-        this.b = b;
-        this.c = c;
-        this.d = d;
+    public static Matrix2 identityMatrix2() {
 
-        return matrix;
+        Matrix2 identityMatrix2 = new Matrix2(1, 0, 0, 1);
+
+        return identityMatrix2;
+    }
+
+    public static Matrix2 rotationMatrix2(double radians) {
+
+        float sine = (float) Math.sin(radians);
+        float cosine = (float) Math.cos(radians);
+
+        Matrix2 rotationMatrix2 = new Matrix2(cosine, -sine, sine, cosine);
+
+        return rotationMatrix2;
+    }
+
+    public static Matrix2 scalingMatrix2(float scalingX, float scalingY) {
+
+        Matrix2 scalingMatrix2 = new Matrix2(scalingX, 0, 0, scalingY);
+        
+        return scalingMatrix2;
     }
 }
